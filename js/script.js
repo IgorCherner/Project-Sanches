6792acbbb7b5d0c54bd71f7726ed5973e550263c
IgorCherner <igorcherner@gmail.com>
2016-12-26 13:42:32 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
 
-	$('input[type="button"]').click(function(e) {
+	$('input[type="button"]').click(function() {
 		var title = $(this).data('title'), 
 		description = $(this).data('description');
 		$('.mod_title').text(title);
 		$('.mod_description').text(description);
+		var src = $(this).data('image');
+		console.log(src);
+		$('#modal_windows .modal-img').attr('src',src);
 	});
 
 	$('input[type="button"]').click(function() {
@@ -15,43 +18,3 @@ $(document).ready(function() {
 		$('#modal_windows').removeClass('modal_windows_open');
 	});
 });
-
-	//----------------------------------------------
-	// $('.modal_details').click(function(){
-	// 	$('#modal_windows').fadeIn('slow');
-	// })
-
-	// $('#modal_close').click(function(){
-	// 	$('#modal_windows').fadeOut('slow');
-	// })
-	
-	// $('.modal_details').click(function() {
-	// 	$('#modal_windows').show(1000);
-	// });
-
-	// $('#modal_close').click(function() {
-	// 	$('#modal_windows').hide(1000);
-	// });
-// ------------------------------------------------------	
-	// $('.modal_details').click(function() {
-	// 	$('#modal_windows').animate({
-	// 		width:  '100%',
-	// 		height: '100%',
-	// 		opacity: 1
-	// 	}, 1000);
-	// });
-
-	// $('#modal_close').click(function() {
-	// 	$('#modal_windows').animate({
-	// 		height: 0
-	// 	}, 1000);
-	// });
-// -------------------------------------------------------
-// $('.modal_details').click(function() {
-	// 	var title = $(this).data('title'),
-	// 	description = $(this).data('description');
-	// 	$('.modal-img-wrapper').html('<h2>'+title+'</h2><p>'+description+'</p>');
-	// });
-	
-
-
